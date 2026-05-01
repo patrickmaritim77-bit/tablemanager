b550686df56e6cc9846708f161ba09d6c45d74e9
Patrick maritim <patrickmaritim77@gmail.com>
2026-05-01 15:09:53 +0300

    Add fallback for MONGO_URI

--- config.py
+++ config.py
@@ -2,5 +2,5 @@
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY', 'super-secret-key-change-in-production')
-    MONGO_URI = os.environ['MONGO_URI']   # Raises KeyError if not set – so you know immediately
+    MONGO_URI = os.environ.get('MONGO_URI', 'mongodb://localhost:27017/tablemanager')
     JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY', 'jwt-secret-change-me')
